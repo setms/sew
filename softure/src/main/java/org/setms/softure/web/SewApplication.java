2a82cf0d8ba65199481b7f8fda3990866ea4aeea
Ray Sinnema <rssinnema@yahoo.com>
2025-07-20 20:43:07 -0600

    Rename package: sew -> softure

--- softure/src/main/java/org/setms/softure/web/SewApplication.java
+++ softure/src/main/java/org/setms/softure/web/SewApplication.java
@@ -1,4 +1,4 @@
-package org.setms.sew.web;
+package org.setms.softure.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
