973363b3bdcfeba4cdbd5031b04ca124e1022e55
Ray Sinnema <rssinnema@yahoo.com>
2026-01-25 07:25:20 -0700

    Remove unused stuff

--- swe/src/main/java/org/setms/swe/domain/model/sdlc/code/java/JavaLanguage.java
+++ swe/src/main/java/org/setms/swe/domain/model/sdlc/code/java/JavaLanguage.java
@@ -7,13 +7,6 @@ import org.setms.swe.domain.model.sdlc.code.ProgrammingLanguage;
 
 public class JavaLanguage implements TopicProvider {
 
-  public static final String BUILD_TOOL = "JavaBuildTool";
-
-  @Override
-  public Set<String> topics() {
-    return Set.of(BUILD_TOOL);
-  }
-
   @Override
   public Map<String, Set<String>> validChoices() {
     return Map.of(ProgrammingLanguage.TOPIC, Set.of("Java"));
