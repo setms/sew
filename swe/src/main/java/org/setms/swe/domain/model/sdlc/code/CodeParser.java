d9d55a47b1259ee13a9d46c3e322785ad58b0e97
Ray Sinnema <rssinnema@yahoo.com>
2026-01-25 09:27:57 -0700

    Implement `CodeParser` and `CodeBuilder`

--- swe/src/main/java/org/setms/swe/domain/model/sdlc/code/CodeParser.java
+++ swe/src/main/java/org/setms/swe/domain/model/sdlc/code/CodeParser.java
@@ -1,7 +1,10 @@
 package org.setms.swe.domain.model.sdlc.code;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.setms.km.domain.model.format.DataString;
 import org.setms.km.domain.model.format.Parser;
 import org.setms.km.domain.model.format.RootObject;
 
@@ -9,6 +12,8 @@ class CodeParser implements Parser {
 
   @Override
   public RootObject parse(InputStream input) throws IOException {
-    throw new UnsupportedOperationException();
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+      return new RootObject(null, null, null).set("code", new DataString(reader.readAllAsString()));
+    }
   }
 }
---
+++ swe/src/test/java/org/setms/swe/domain/model/sdlc/code/CodeFormatTest.java
@@ -0,0 +1,40 @@
+package org.setms.swe.domain.model.sdlc.code;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.setms.km.domain.model.format.DataString;
+import org.setms.km.domain.model.format.RootObject;
+
+class CodeFormatTest {
+
+  private static final String CODE =
+      """
+      language: MadeUp;
+      package:  root;
+      code:     begin print("Hello, world!"); end
+      """;
+
+  private final CodeFormat codeFormat = CodeFormat.INSTANCE;
+
+  @Test
+  void shouldParseCode() throws IOException {
+    var actual = codeFormat.newParser().parse(new ByteArrayInputStream(CODE.getBytes(UTF_8)));
+
+    assertThat(actual.property("code", DataString.class).getValue()).isEqualTo(CODE);
+  }
+
+  @Test
+  void shouldBuildCode() throws IOException {
+    var code = new RootObject(null, null, null).set("code", new DataString(CODE));
+    var output = new ByteArrayOutputStream();
+
+    codeFormat.newBuilder().build(code, output);
+
+    assertThat(output).hasToString(CODE);
+  }
+}
