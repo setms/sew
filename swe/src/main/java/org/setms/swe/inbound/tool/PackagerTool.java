b8a772750e4d74e6cf65db0d6bb60dbf546849e5
Ray Sinnema <rssinnema@yahoo.com>
2026-03-07 09:58:10 -0700

    feat: add PackagerTool that calls CodePackager.packageCode() during validation

---
+++ swe/src/main/java/org/setms/swe/inbound/tool/PackagerTool.java
@@ -0,0 +1,46 @@
+package org.setms.swe.inbound.tool;
+
+import java.util.Collection;
+import java.util.Set;
+import org.setms.km.domain.model.artifact.Artifact;
+import org.setms.km.domain.model.tool.ArtifactTool;
+import org.setms.km.domain.model.tool.Input;
+import org.setms.km.domain.model.tool.ResolvedInputs;
+import org.setms.km.domain.model.validation.Diagnostic;
+import org.setms.km.domain.model.workspace.Resource;
+import org.setms.swe.domain.model.sdlc.code.CodeArtifact;
+import org.setms.swe.domain.model.sdlc.technology.TechnologyResolver;
+
+public class PackagerTool extends ArtifactTool<CodeArtifact> {
+
+  private final TechnologyResolver resolver;
+
+  public PackagerTool() {
+    this(new TechnologyResolverImpl());
+  }
+
+  PackagerTool(TechnologyResolver resolver) {
+    this.resolver = resolver;
+  }
+
+  @Override
+  public Set<Input<? extends CodeArtifact>> validationTargets() {
+    return Set.of();
+  }
+
+  @Override
+  public Set<Input<? extends Artifact>> validationContext() {
+    return Set.of();
+  }
+
+  @Override
+  public void validate(
+      Resource<?> resource,
+      CodeArtifact artifact,
+      ResolvedInputs inputs,
+      Collection<Diagnostic> diagnostics) {
+    resolver
+        .codePackager(inputs, diagnostics)
+        .ifPresent(packager -> packager.packageCode(resource.select("/"), diagnostics));
+  }
+}
