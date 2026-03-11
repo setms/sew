e775f14936bfa0fbaba72d1cdaa02bae0994f64f
Ray Sinnema <rssinnema@yahoo.com>
2026-03-11 08:04:49 -0600

    feat: add EntityTool.validationContext() with database schema inputs

--- swe/src/main/java/org/setms/swe/inbound/tool/EntityTool.java
+++ swe/src/main/java/org/setms/swe/inbound/tool/EntityTool.java
@@ -1,8 +1,11 @@
 package org.setms.swe.inbound.tool;
 
+import static org.setms.swe.inbound.tool.Inputs.databaseSchemas;
 import static org.setms.swe.inbound.tool.Inputs.entities;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.setms.km.domain.model.artifact.Artifact;
 import org.setms.km.domain.model.tool.ArtifactTool;
 import org.setms.km.domain.model.tool.Input;
 import org.setms.swe.domain.model.sdlc.design.Entity;
@@ -12,5 +15,10 @@ public class EntityTool extends ArtifactTool<Entity> {
   @Override
   public Set<Input<? extends Entity>> validationTargets() {
     return Set.of(entities());
+  }
+
+  @Override
+  public Set<Input<? extends Artifact>> validationContext() {
+    return new HashSet<>(databaseSchemas());
   }
 }
--- swe/src/test/java/org/setms/swe/inbound/tool/EntityToolTest.java
+++ swe/src/test/java/org/setms/swe/inbound/tool/EntityToolTest.java
@@ -4,13 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.setms.swe.domain.model.sdlc.design.FieldConstraint.NONEMPTY;
 import static org.setms.swe.domain.model.sdlc.design.FieldType.TEXT;
 
+import java.util.Set;
 import org.setms.km.domain.model.artifact.Artifact;
+import org.setms.km.domain.model.tool.Input;
 import org.setms.swe.domain.model.sdlc.design.Entity;
 
 class EntityToolTest extends ToolTestCase<Entity> {
 
   public EntityToolTest() {
     super(new EntityTool(), Entity.class, "main/design");
+  }
+
+  @Override
+  protected void assertValidationContext(Set<Input<? extends Artifact>> inputs) {
+    assertThat(inputs)
+        .as("Validation context includes database schemas")
+        .anyMatch(input -> input.matches("src/main/database/shop/product.sql"));
   }
 
   @Override
