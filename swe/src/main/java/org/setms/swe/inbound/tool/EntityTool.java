e0acc248dd2f6fbf3a5e19160037fea6124fecd9
Ray Sinnema <rssinnema@yahoo.com>
2026-03-12 07:00:55 -0600

    style: fix method order and add doApply for schema generation

--- swe/src/main/java/org/setms/swe/inbound/tool/EntityTool.java
+++ swe/src/main/java/org/setms/swe/inbound/tool/EntityTool.java
@@ -1,20 +1,26 @@
 package org.setms.swe.inbound.tool;
 
+import static org.setms.km.domain.model.tool.AppliedSuggestion.created;
+import static org.setms.km.domain.model.tool.AppliedSuggestion.failedWith;
 import static org.setms.km.domain.model.validation.Level.WARN;
 import static org.setms.swe.inbound.tool.Inputs.databaseSchemas;
 import static org.setms.swe.inbound.tool.Inputs.decisions;
 import static org.setms.swe.inbound.tool.Inputs.entities;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.setms.km.domain.model.artifact.Artifact;
+import org.setms.km.domain.model.tool.AppliedSuggestion;
 import org.setms.km.domain.model.tool.ArtifactTool;
 import org.setms.km.domain.model.tool.Input;
 import org.setms.km.domain.model.tool.ResolvedInputs;
 import org.setms.km.domain.model.validation.Diagnostic;
+import org.setms.km.domain.model.validation.Location;
 import org.setms.km.domain.model.validation.Suggestion;
+import org.setms.km.domain.model.workspace.Resource;
 import org.setms.swe.domain.model.sdlc.database.DatabaseSchema;
 import org.setms.swe.domain.model.sdlc.design.Entity;
 import org.setms.swe.domain.model.sdlc.technology.TechnologyResolver;
@@ -52,6 +58,11 @@ public class EntityTool extends ArtifactTool<Entity> {
         .ifPresent(database -> diagnostics.add(missingSchemaFor(entity)));
   }
 
+  private boolean hasDatabaseSchema(Entity entity, ResolvedInputs inputs) {
+    return inputs.get(DatabaseSchema.class).stream()
+        .anyMatch(schema -> schema.getName().equalsIgnoreCase(entity.getName()));
+  }
+
   private Diagnostic missingSchemaFor(Entity entity) {
     return new Diagnostic(
         WARN,
@@ -60,8 +71,39 @@ public class EntityTool extends ArtifactTool<Entity> {
         new Suggestion(GENERATE_SCHEMA, "Generate database schema"));
   }
 
-  private boolean hasDatabaseSchema(Entity entity, ResolvedInputs inputs) {
-    return inputs.get(DatabaseSchema.class).stream()
-        .anyMatch(schema -> schema.getName().equalsIgnoreCase(entity.getName()));
+  @Override
+  protected AppliedSuggestion doApply(
+      Resource<?> resource,
+      Entity entity,
+      String suggestionCode,
+      Location location,
+      ResolvedInputs inputs) {
+    return switch (suggestionCode) {
+      case GENERATE_SCHEMA -> generateSchemaFor(resource, entity, inputs);
+      default -> AppliedSuggestion.unknown(suggestionCode);
+    };
+  }
+
+  private AppliedSuggestion generateSchemaFor(
+      Resource<?> resource, Entity entity, ResolvedInputs inputs) {
+    return resolver
+        .database(inputs, new ArrayList<>())
+        .map(database -> writeSchema(database.schemaFor(entity), resource))
+        .orElseGet(AppliedSuggestion::none);
+  }
+
+  private AppliedSuggestion writeSchema(DatabaseSchema schema, Resource<?> resource) {
+    try {
+      var path = schema.getPackage().replace('.', '/');
+      var target =
+          resource
+              .select("/src/main/design/physical")
+              .select(path)
+              .select(schema.getName() + ".sql");
+      target.writeAsString(schema.getCode());
+      return created(target);
+    } catch (Exception e) {
+      return failedWith(e);
+    }
   }
 }
