746467ec22c1d98f4e83ca088fb48386e5f82b12
Ray Sinnema <rssinnema@yahoo.com>
2026-03-22 10:52:56 +0100

    feat: ServerSideHtmlGenerator generates CSS and HTML artifacts

--- swe/src/main/java/org/setms/swe/domain/model/sdlc/code/html/ServerSideHtmlGenerator.java
+++ swe/src/main/java/org/setms/swe/domain/model/sdlc/code/html/ServerSideHtmlGenerator.java
@@ -1,17 +1,66 @@
 package org.setms.swe.domain.model.sdlc.code.html;
 
-import static java.util.Collections.emptyList;
-
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import org.setms.km.domain.model.artifact.FullyQualifiedName;
 import org.setms.swe.domain.model.sdlc.code.CodeArtifact;
 import org.setms.swe.domain.model.sdlc.technology.UiGenerator;
 import org.setms.swe.domain.model.sdlc.ui.DesignSystem;
+import org.setms.swe.domain.model.sdlc.ui.Property;
+import org.setms.swe.domain.model.sdlc.ui.Style;
+import org.setms.swe.domain.model.sdlc.ux.Container;
 import org.setms.swe.domain.model.sdlc.ux.Wireframe;
 
 public class ServerSideHtmlGenerator implements UiGenerator {
 
   @Override
   public List<CodeArtifact> generate(Wireframe wireframe, DesignSystem designSystem) {
-    return emptyList();
+    return List.of(cssFor(designSystem), htmlFor(wireframe));
+  }
+
+  private CodeArtifact cssFor(DesignSystem designSystem) {
+    var code =
+        Optional.ofNullable(designSystem.getStyles()).stream()
+            .flatMap(Collection::stream)
+            .map(this::cssRule)
+            .reduce("", (a, b) -> a + b);
+    return new CodeArtifact(new FullyQualifiedName("", designSystem.getName())).setCode(code);
+  }
+
+  private String cssRule(Style style) {
+    var properties =
+        Optional.ofNullable(style.getProperties()).stream()
+            .flatMap(Collection::stream)
+            .map(this::cssProperty)
+            .reduce("", (a, b) -> a + b);
+    return ".%s {\n%s}\n".formatted(style.getName(), properties);
+  }
+
+  private String cssProperty(Property property) {
+    return "  %s: %s;\n".formatted(property.getName(), property.getValue());
+  }
+
+  private CodeArtifact htmlFor(Wireframe wireframe) {
+    var containers =
+        Optional.ofNullable(wireframe.getContainers()).stream()
+            .flatMap(Collection::stream)
+            .map(this::htmlDiv)
+            .reduce("", (a, b) -> a + b);
+    var code =
+        """
+        <!DOCTYPE html>
+        <html>
+        <head><title>%s</title></head>
+        <body>
+        %s</body>
+        </html>
+        """
+            .formatted(wireframe.getName(), containers);
+    return new CodeArtifact(new FullyQualifiedName("", wireframe.getName())).setCode(code);
+  }
+
+  private String htmlDiv(Container container) {
+    return "<div id=\"%s\"></div>\n".formatted(container.getName());
   }
 }
---
+++ swe/src/test/java/org/setms/swe/domain/model/sdlc/code/html/ServerSideHtmlGeneratorTest.java
@@ -0,0 +1,75 @@
+package org.setms.swe.domain.model.sdlc.code.html;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.setms.km.domain.model.artifact.FullyQualifiedName;
+import org.setms.swe.domain.model.sdlc.code.CodeArtifact;
+import org.setms.swe.domain.model.sdlc.ui.DesignSystem;
+import org.setms.swe.domain.model.sdlc.ui.Property;
+import org.setms.swe.domain.model.sdlc.ui.Style;
+import org.setms.swe.domain.model.sdlc.ux.Container;
+import org.setms.swe.domain.model.sdlc.ux.Wireframe;
+
+class ServerSideHtmlGeneratorTest {
+
+  private final ServerSideHtmlGenerator generator = new ServerSideHtmlGenerator();
+
+  @Test
+  void shouldGenerateCssForDesignSystem() {
+    var designSystem = givenDesignSystemWithButtonStyle();
+    var wireframe = new Wireframe(new FullyQualifiedName("ux", "Checkout"));
+
+    var actual = generator.generate(wireframe, designSystem);
+
+    assertThatCssArtifactContainsButtonStyle(actual);
+  }
+
+  private DesignSystem givenDesignSystemWithButtonStyle() {
+    return new DesignSystem(new FullyQualifiedName("ui", "Styles"))
+        .setStyles(
+            List.of(
+                new Style(new FullyQualifiedName("ui", "button"))
+                    .setProperties(
+                        List.of(
+                            new Property(new FullyQualifiedName("", "color")).setValue("red")))));
+  }
+
+  private void assertThatCssArtifactContainsButtonStyle(List<CodeArtifact> artifacts) {
+    assertThat(artifacts)
+        .as("CSS artifact for DesignSystem")
+        .anySatisfy(
+            artifact ->
+                assertThat(artifact.getCode())
+                    .as("CSS code with '.button { color: red; }'")
+                    .contains(".button")
+                    .contains("color: red"));
+  }
+
+  @Test
+  void shouldGenerateHtmlForWireframe() {
+    var wireframe = givenWireframeWithMainContainer();
+    var designSystem = new DesignSystem(new FullyQualifiedName("ui", "Styles"));
+
+    var actual = generator.generate(wireframe, designSystem);
+
+    assertThatHtmlArtifactContainsMainContainer(actual);
+  }
+
+  private Wireframe givenWireframeWithMainContainer() {
+    return new Wireframe(new FullyQualifiedName("ux", "Checkout"))
+        .setContainers(List.of(new Container(new FullyQualifiedName("", "main"))));
+  }
+
+  private void assertThatHtmlArtifactContainsMainContainer(List<CodeArtifact> artifacts) {
+    assertThat(artifacts)
+        .as("HTML artifact for Wireframe")
+        .anySatisfy(
+            artifact ->
+                assertThat(artifact.getCode())
+                    .as("HTML code containing 'main' container")
+                    .contains("<html>")
+                    .contains("main"));
+  }
+}
